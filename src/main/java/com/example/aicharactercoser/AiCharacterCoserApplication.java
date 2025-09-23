02db41383724b74819033795aa44c92d83653868
emialyx <15770877537@163.com>
2025-09-23 19:05:43 +0800

    9.23 项目初式化依赖添加
    1.spring web 依赖
    2.lombok 依赖
    3.mysql 数据库依赖
    4.mybatis-plus 依赖
    5.hutool 工具依赖
    6.knife4j+swagger 接口测试工具依赖

---
+++ src/main/java/com/example/aicharactercoser/AiCharacterCoserApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicharactercoser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCharacterCoserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCharacterCoserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aicharactercoser/AiCharacterCoserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicharactercoser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCharacterCoserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
